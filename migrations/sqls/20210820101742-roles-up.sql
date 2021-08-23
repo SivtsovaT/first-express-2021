dab24f6adc2b628df6ccf47a7fca779b75c9d0d4
SivtsovaT <sivtsovatanya1985@gmail.com>
2021-08-23 18:36:20 +0300

    Format javascript sources in project
    - fixed spaces for SQL

--- migrations/sqls/20210820101742-roles-up.sql
+++ migrations/sqls/20210820101742-roles-up.sql
@@ -1,1 +1,2 @@
-alter table  first_express.user_credentials add column roles text[] not null default '{}';
+alter table first_express.user_credentials
+    add column roles text[] not null default '{}';
